061e6cd20f40fc35d45aa9c3c7001f8292c9e5ed
yindaheng98 <yindaheng98@gmail.com>
2026-02-09 20:36:43 -0800

    rename

--- feature_3dgs/dataset/dataset.py
+++ feature_3dgs/dataset/dataset.py
@@ -6,25 +6,25 @@ from gaussian_splatting.dataset import CameraDataset
 from feature_3dgs.extractor import AbstractFeatureExtractor
 
 
-# Dynamically create FeaturedCamera as a proper NamedTuple
+# Dynamically create FeatureCamera as a proper NamedTuple
 # with all Camera fields + feature_map
-FeaturedCamera = NamedTuple('FeaturedCamera', [
+FeatureCamera = NamedTuple('FeatureCamera', [
     (name, Camera.__annotations__[name]) for name in Camera._fields
 ] + [('feature_map', torch.Tensor)])
-FeaturedCamera.__new__.__defaults__ = (
+FeatureCamera.__new__.__defaults__ = (
     *Camera._field_defaults.values(),
     None,  # feature_map default
 )
 
 
-class FeaturedCameraDataset(CameraDataset):
+class FeatureCameraDataset(CameraDataset):
 
     def __init__(self, cameras: CameraDataset, feature_extractor: AbstractFeatureExtractor):
         self.cameras = cameras
         self.feature_extractor = feature_extractor
         self.feature_map_cache = [None] * len(cameras)
 
-    def to(self, device) -> 'FeaturedCameraDataset':
+    def to(self, device) -> 'FeatureCameraDataset':
         self.cameras.to(device)
         self.feature_extractor.to(device)
         self.feature_map_cache = [(feature_map.to(device) if feature_map is not None else None) for feature_map in self.feature_map_cache]
@@ -33,11 +33,11 @@ class FeaturedCameraDataset(CameraDataset):
     def __len__(self) -> int:
         return len(self.cameras)
 
-    def __getitem__(self, idx) -> FeaturedCamera:
+    def __getitem__(self, idx) -> FeatureCamera:
         camera = self.cameras[idx]
         feature_map = None
         if camera.ground_truth_image is not None:
             if self.feature_map_cache[idx] is None:
                 self.feature_map_cache[idx] = self.feature_extractor(camera.ground_truth_image)
             feature_map = self.feature_map_cache[idx]
-        return FeaturedCamera(*camera, feature_map=feature_map)
+        return FeatureCamera(*camera, feature_map=feature_map)
