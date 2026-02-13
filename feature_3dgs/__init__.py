e66080af1fc9f84f6730fffb70ea9fbe0c210775
yindaheng98 <yindaheng98@gmail.com>
2026-02-12 16:57:06 -0800

    fixed export

--- feature_3dgs/__init__.py
+++ feature_3dgs/__init__.py
@@ -1,3 +1,4 @@
-from .gaussian_model import FeatureGaussianModel, AbstractDecoder
+from .gaussian_model import FeatureGaussianModel
+from .decoder import AbstractDecoder
 from .dataset import FeatureCamera, FeatureCameraDataset, AbstractFeatureExtractor
 from .combinations import available_datasets, build_dataset
