ef9c6036566b5881f270552432f50590100bcec7
yindaheng98 <yindaheng98@gmail.com>
2026-02-12 19:33:22 -0800

    export registry

--- feature_3dgs/__init__.py
+++ feature_3dgs/__init__.py
@@ -2,3 +2,4 @@ from .gaussian_model import FeatureGaussianModel
 from .decoder import AbstractDecoder, NoopDecoder
 from .dataset import FeatureCamera, FeatureCameraDataset, AbstractFeatureExtractor
 from .combinations import available_datasets, build_dataset
+from .registry import register_extractor_decoder, get_available_extractor_decoders, build_extractor_decoder
