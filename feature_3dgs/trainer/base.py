eec08f21ab02c03aac0e91cfc48856d8f1c2d5c2
yindaheng98 <yindaheng98@gmail.com>
2026-02-14 16:29:21 -0800

    BaseDensificationTrainer

--- feature_3dgs/trainer/base.py
+++ feature_3dgs/trainer/base.py
@@ -3,7 +3,8 @@ from gaussian_splatting.dataset import TrainableCameraDataset
 from gaussian_splatting.trainer.camera_trainable import CameraTrainerWrapper
 from gaussian_splatting.trainer.depth import DepthTrainer, DepthTrainerWrapper
 
-from .trainer import BaseSemanticTrainer as BaseTrainer
+from .trainer import BaseSemanticTrainer as BaseTrainer, SemanticTrainerWrapper
+from .densifier import BaseSemanticDensificationTrainer
 
 
 def BaseCameraTrainer(
@@ -19,3 +20,16 @@ def BaseCameraTrainer(
 
 def BaseDepthTrainer(model: GaussianModel, scene_extent: float, *args, **kwargs) -> DepthTrainer:
     return DepthTrainerWrapper(BaseTrainer, model, scene_extent, *args, **kwargs)
+
+
+def BaseDensificationTrainer(
+        model: GaussianModel,
+        scene_extent: float,
+        semantic_lr: float = 0.001,
+        semantic_decoder_lr: float = 0.0001,
+        semantic_loss_weight: float = 1,
+        *args, **kwargs) -> BaseSemanticDensificationTrainer:
+    return SemanticTrainerWrapper(
+        lambda model, scene_extent, dataset, *args, **kwargs: BaseSemanticDensificationTrainer(model, scene_extent, *args, **kwargs),
+        model, scene_extent, semantic_lr=semantic_lr, semantic_decoder_lr=semantic_decoder_lr, semantic_loss_weight=semantic_loss_weight,
+    )
