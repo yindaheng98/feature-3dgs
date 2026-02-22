010f5574fb5fee1789c575d4008cdebf75f9cde1
yindaheng98 <yindaheng98@gmail.com>
2026-02-21 20:20:31 -0800

    export AbstractFeatureDecoder

--- feature_3dgs/__init__.py
+++ feature_3dgs/__init__.py
@@ -1,4 +1,4 @@
-from .gaussian_model import SemanticGaussianModel, CameraTrainableSemanticGaussianModel
+from .gaussian_model import SemanticGaussianModel, CameraTrainableSemanticGaussianModel, AbstractFeatureDecoder
 from .decoder import AbstractTrainableFeatureDecoder, NoopFeatureDecoder
 from .extractor import AbstractFeatureExtractor, FeatureCameraDataset, TrainableFeatureCameraDataset
 from .registry import register_extractor_decoder, get_available_extractor_decoders, build_extractor_decoder
