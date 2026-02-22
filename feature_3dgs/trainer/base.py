811ed3d71a1f555215b453c41093d69cfcac0a32
yindaheng98 <yindaheng98@gmail.com>
2026-02-21 22:07:58 -0800

    simplify

--- feature_3dgs/trainer/base.py
+++ feature_3dgs/trainer/base.py
@@ -7,29 +7,13 @@ from .trainer import BaseSemanticTrainer as BaseTrainer, SemanticTrainerWrapper
 from .densifier import BaseSemanticDensificationTrainer
 
 
-def BaseCameraTrainer(
-        model: CameraTrainableSemanticGaussianModel,
-        dataset: TrainableFeatureCameraDataset,
-        **configs):
-    return CameraTrainerWrapper(
-        lambda model, dataset, **configs: BaseTrainer(model, dataset, **configs),
-        model, dataset, **configs
-    )
+def BaseCameraTrainer(model: CameraTrainableSemanticGaussianModel, dataset: TrainableFeatureCameraDataset, **configs):
+    return CameraTrainerWrapper(BaseTrainer, model, dataset, **configs)
 
 
 def BaseDepthTrainer(model: SemanticGaussianModel, dataset: FeatureCameraDataset, **configs) -> DepthTrainer:
     return DepthTrainerWrapper(BaseTrainer, model, dataset, **configs)
 
 
-def BaseDensificationTrainer(
-        model: SemanticGaussianModel,
-        dataset: FeatureCameraDataset,
-        semantic_lr: float = 0.001,
-        semantic_decoder_lr: float = 0.0001,
-        semantic_loss_weight: float = 1,
-        **configs):
-    return SemanticTrainerWrapper(
-        lambda model, dataset, **configs: BaseSemanticDensificationTrainer(model, dataset, **configs),
-        model, dataset, semantic_lr=semantic_lr, semantic_decoder_lr=semantic_decoder_lr, semantic_loss_weight=semantic_loss_weight,
-        **configs
-    )
+def BaseDensificationTrainer(model: SemanticGaussianModel, dataset: FeatureCameraDataset, **configs):
+    return SemanticTrainerWrapper(BaseSemanticDensificationTrainer, model, dataset, **configs)
