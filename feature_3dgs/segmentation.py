cfe87a0a72f295d26afd144fd9d2f36971262aea
yindaheng98 <yindaheng98@gmail.com>
2026-02-22 20:39:01 -0800

    init segmentation from render

---
+++ feature_3dgs/segmentation.py
@@ -0,0 +1,36 @@
+import os
+
+import torch
+from feature_3dgs import get_available_extractor_decoders
+from feature_3dgs.render import prepare_rendering, rendering
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--sh_degree", default=3, type=int)
+    parser.add_argument("--name", choices=get_available_extractor_decoders(), required=True, type=str)
+    parser.add_argument("--embed_dim", required=True, type=int)
+    parser.add_argument("-s", "--source", required=True, type=str)
+    parser.add_argument("-d", "--destination", required=True, type=str)
+    parser.add_argument("-i", "--iteration", required=True, type=int)
+    parser.add_argument("--load_camera", default=None, type=str)
+    parser.add_argument("--mode", choices=["base", "camera"], default="base")
+    parser.add_argument("--device", default="cuda", type=str)
+    parser.add_argument("--dataset_cache_device", default="cpu", type=str)
+    parser.add_argument("--no_image_mask", action="store_true")
+    parser.add_argument("-e", "--option_extractor", default=[], action='append', type=str)
+    args = parser.parse_args()
+    load_ply = os.path.join(args.destination, "point_cloud", "iteration_" + str(args.iteration), "point_cloud.ply")
+    save = os.path.join(args.destination, "ours_{}".format(args.iteration))
+    extractor_configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option_extractor}
+    with torch.no_grad():
+        dataset, gaussians = prepare_rendering(
+            name=args.name, sh_degree=args.sh_degree,
+            source=args.source, embed_dim=args.embed_dim,
+            device=args.device, dataset_cache_device=args.dataset_cache_device,
+            trainable_camera=args.mode == "camera",
+            load_ply=load_ply, load_camera=args.load_camera,
+            load_mask=not args.no_image_mask,
+            extractor_configs=extractor_configs)
+        rendering(dataset, gaussians, save)
