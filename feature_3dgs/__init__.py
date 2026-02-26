8d626ebf4a11a85834460eb68cd824b7d2840da8
yindaheng98 <yindaheng98@gmail.com>
2026-02-25 20:46:55 -0800

    noop decoder is useless

--- feature_3dgs/__init__.py
+++ feature_3dgs/__init__.py
@@ -1,5 +1,5 @@
 from .gaussian_model import SemanticGaussianModel, CameraTrainableSemanticGaussianModel
-from .decoder import AbstractFeatureDecoder, AbstractTrainableFeatureDecoder, NoopFeatureDecoder
+from .decoder import AbstractFeatureDecoder, AbstractTrainableFeatureDecoder, LinearDecoder
 from .extractor import AbstractFeatureExtractor, FeatureCameraDataset, TrainableFeatureCameraDataset
 from .registry import register_extractor_decoder, get_available_extractor_decoders, build_extractor_decoder
 from . import dinov3
--- feature_3dgs/decoder/__init__.py
+++ feature_3dgs/decoder/__init__.py
@@ -1,4 +1,3 @@
 from .abc import AbstractFeatureDecoder
 from .trainable import AbstractTrainableFeatureDecoder
-from .noop import NoopFeatureDecoder
 from .linear import LinearDecoder
