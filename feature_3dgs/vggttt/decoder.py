ee7053412b29d0ebbfea5cbd3296a6ce96911e51
yindaheng98 <yindaheng98@gmail.com>
2026-08-12 22:21:53 -0700

    VGGTTTLinearAvgDecoder

---
+++ feature_3dgs/vggttt/decoder.py
@@ -0,0 +1,6 @@
+from feature_3dgs.vggt.decoder import VGGTLinearAvgDecoder
+
+
+class VGGTTTLinearAvgDecoder(VGGTLinearAvgDecoder):
+    """Decoder that aligns Gaussian features with VGGTTTExtractor output.
+    """
